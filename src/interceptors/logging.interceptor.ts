c3720564a964b1ae2e29a22921a517e495748738
AndersonAndrad <anderson_andrade_@outlook.com>
2021-02-05 23:50:14 -0300

    implementing the swagger

--- src/interceptors/logging.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-
-@Injectable()
-export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHander): Observable<any> {}
-}
